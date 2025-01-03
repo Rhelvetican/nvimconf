bdd512403a19efc8bf38684783b62da73882d3e5
Rh4096 <rhelvetican@gmail.com>
2025-01-04 03:27:15 +0700

    resentment2

--- lua/plugins/configs/mason.lua
+++ lua/plugins/configs/mason.lua
@@ -2,12 +2,7 @@ local map = vim.keymap.set
 
 map({ "n", "o", "v" }, "<leader>m", "<cmd>Mason<CR>")
 
--- add binaries installed by mason.nvim to path
-local is_windows = vim.uv.os_uname().sysname == "Windows_NT"
-vim.env.PATH = vim.fn.stdpath("data") .. "/mason/bin" .. (is_windows and "; " or ":") .. vim.env.PATH
-
 require("mason").setup({
-	PATH = "prepend",
 	ensure_installed = {
 		"ast-grep",
 		"bacon",
