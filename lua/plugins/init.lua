b1d0a15ea2648c1107c1938c44660c3c97717d9b
Rh4096 <rhelvetican@gmail.com>
2025-01-08 22:30:40 +0700

    fix bug

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -166,7 +166,11 @@ return {
 
 			completion = {
 				keyword = { range = "full" },
-				ghost_text = { enabled = true },
+			},
+
+			documentation = {
+				auto_show = true,
+				auto_show_delay_ms = 500,
 			},
 
 			signature = { enabled = true, window = { treesitter_highlighting = false } },
